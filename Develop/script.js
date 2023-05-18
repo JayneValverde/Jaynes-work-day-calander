36491e08975d257fa8409209800cdc53451e3cbd
Jayne Valverde <jaynevalverde@gmail.com>
2023-05-18 17:07:30 -0500

    read me addition and edits

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 // $(function () {
 
+// Save reference to import DOM elements 
 var timeDisplayEl = $('#time-display');
 let textarea;
-let workingHours = [9,10,11,12,13,14,15,16,17,18,19]
+let workingHours = [9,10,11,12,13,14,15,16,17,18,19,]
 
- // handle displaying the time 
+ // Function to display the time 
 function displayTime() {
   var rightNow = dayjs(); 
   timeDisplayEl.text(rightNow.format('MMM DD YYYY [at] hh:mm:ss a'));
   }
-  
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage.
+  // local storage. 
 function saveText(hour) {
   textarea=document.getElementById(`text-${hour}`)
   localStorage.setItem(`element-${hour}`, textarea.value)
 }
 
-
+// Loop through working hours to store text in localStorage
 for(var x = 0; x < workingHours.length; x++) {
   textarea=document.getElementById(`text-${workingHours[x]}`)
   textarea.innerHTML = localStorage.getItem(`element-${workingHours[x]}`)
-  
 }
 
-
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. 
   setInterval(displayTime,1000);
   function updateTimeBlocks() {
     var timeBlocks = $('.time-block');
-  
+
+  // loop through timeBlocks to determine dayHour
     for(var i = 0; i < timeBlocks.length; i++) {
       var dayHour = dayjs().hour();
       var blockId = $(timeBlocks[i]).attr('id');
@@ -63,19 +54,13 @@ for(var x = 0; x < workingHours.length; x++) {
         $(timeBlocks[i]).removeClass('present');
         $(timeBlocks[i]).addClass('future');
       }
-
     }
   }
-  
+
   updateTimeBlocks();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-  // displayTime();
+// Updates time display
+  displayTime();
 
 // });
 
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Jaynes-work-day-calander
-a daily planner to create a schedule 
+## Jayné M. Valverde
+Link to deployed work day calander
+
+
+## Work Day Calander 
+* Upon opening launching the app you are presented with a list of standard workday hours (9am-5pm)
+* In the header of the app you will the current date and time that stays current as you use the app. 
+    * this is to help the user keep track of the time while they are planning their day. 
+* The **text-areas** are color coded depending on the time of day. 
+    * The past: Grey 
+    * The present: Red
+    * The future: Green 
+* Once the user inputs their task for the day and presses the button on the right, their tasks will be saved to their local storage. 
+    * When they referesh the page all of their saved tasks will remain so they can successfully rememener all the tasks that need to be done for their work day. 
+
+
+
+## Scheduler App Preview
+![screenshot one](./Assets/workday.png)
+
+
+## Resources 
+
+
+**Corrando Alfano**: <br>
+He was my tutor for the week and he helped me a great deal with setting up my local storage. He guided me to really take a look at how I set up my HTML code and to maybe build this code into jQuery. This is deff something I will be trying on my spare time. 
+
+
+**Andrew Truong (TA)**: <br>
+lately I have been having a hard time voiceing my concernes about how behind I feel. Espcially when it comes to terminology with new concepts. He was really patient with me during one of the office hours when I asked him to explain the logic of setting up a funciton and the **for** loops required for the part of the assignment. 
+
+
